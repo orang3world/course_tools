8087e7988682dac39f761477a218c8dc34ed6bc9
orang3world <aaorange75@gmail.com>
2024-04-16 21:57:38 -0300

    descuento de 10 segundos para recuperar el tiempo de escritura e interpretacion

--- timestamp.py
+++ timestamp.py
@@ -1,10 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 from pathlib import Path
 from zmq import NULL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
 
 
 # screen clear function
@@ -29,65 +28,63 @@ screenClean()
 with open(f"rec_{dte}.txt", "a+") as f:
     f.write(f'Recording date: {start_time.strftime("%Y-%m-%d %H:%M:%S")}\n\n')
     f.write("00:00:00 Inicio de la clase\n")
-    
+
 # variable with current directory's path
-BASE_DIR = Path('rec_{dte}.txt').resolve().parent
+BASE_DIR = Path("rec_{dte}.txt").resolve().parent
 
 # while (infinity loop)
 while True:
-    '''
+    """
         file creation or write a line if file exist
         file's name has format: 'rec_yyyymmdd.txt\' 
-    '''
+    """
     with open(f"rec_{dte}.txt", "a+") as f:
-        
-        # note or exit           
+        # note or exit
         note = input("Write your section's name ('e' for exit or 'p' for pause):\n")
-        lap = datetime.now()
+        lap = datetime.now() - timedelta(seconds=10)
         delta = lap - start_time
         lastTime = origen_time + delta
-        
+
         # after a pause
         if pauseDelta != NULL:
-            pauseTime = (delta-pauseDelta)
+            pauseTime = delta - pauseDelta
             start_time += pauseTime
             lastTime -= pauseTime
             pauseDelta = NULL
-            newLine = f'{note}\n'
+            newLine = f"{note}\n"
         # whitout pause
-        else :
+        else:
             newLine = f'{lastTime.strftime("%H:%M:%S")} {note}\n'
 
         # exit option
         if note == "e" or note == "E":
-            f.write(f'\nThe file\'s path is: {BASE_DIR}/rec_{dte}.txt')
+            f.write(f"\nThe file's path is: {BASE_DIR}/rec_{dte}.txt")
             f.seek(0)
             content = f.read()
             screenClean()
             print(content)
             break
-        
+
         # pause option
         elif note == "p" or note == "P":
             pauseDelta = delta
             newLine = f'{lastTime.strftime("%H:%M:%S")} Recording Paused... '
             f.write(newLine)
-            
+
         # note option
         else:
             # print to file
             f.write(newLine)
-            
-    # clear the screen and show the file content for each  of the iterations
-    #with open(f"rec_{dte}.txt", "r") as f:
+
+        # clear the screen and show the file content for each  of the iterations
+        # with open(f"rec_{dte}.txt", "r") as f:
         f.seek(0)
         content = f.read()
         screenClean()
         print(content)
-        
+
 # final display the content of the file on the screen
 with open(f"rec_{dte}.txt", "r") as f:
     content = f.read()
     screenClean()
     print(content)
-
